7503802e4fcad5862c528b7f8d395546e19bbb5a
ozge sanli <ozgezeynep08@gmail.com>
2019-08-18 15:36:08 +0200

    all pages fixed

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -21,10 +21,9 @@ class CocktailsController < ApplicationController
     end
   end
 
-
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :photo)
+    params.require(:cocktail).permit(:name,:description, :photo)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_16_131234) do
+ActiveRecord::Schema.define(version: 2019_08_17_120927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_08_16_131234) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.string "description"
   end
 
   create_table "doses", force: :cascade do |t|
