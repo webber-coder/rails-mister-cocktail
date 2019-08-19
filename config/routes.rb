b04113790bc0ba0a39f33e666ab576ae377cf7f6
ozge <ozgezeynep08@gmail.com>
2019-08-19 22:56:19 +0200

    root fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :new, :show, :create] do
+  resources :cocktails, only: [:new, :show, :create] do
     resources :doses, only: [:new, :create]
   end
+  root to: "cocktails#index"
   resources :doses, only: [:destroy]
   resources :ingredients, only: [:index, :new, :show, :create]
 end
